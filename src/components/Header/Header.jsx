d6e5500366c85020bc1dcc927f1d65aff9e41b39
h-samara-mohsin <samaramohsin4005617@gmail.com>
2026-04-10 23:21:39 +0500

    feat(task1): build out Header with search and user avatar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,38 @@
+import { Bell, Search } from "lucide-react";
+import './Header.css'
 
-export default function Header() {
+export default function Header({ onMenuToggle }) {
     return (
-        <header style={{ padding: '16px 24px', background: '#fff', borderBottom: '1px solid #e5e7eb'}}>
-            <h1>Header</h1>
+        <header className="header">
+            {/* Left — hamburger on mobile */}
+            <button className="menu-toggle" onClick={onMenuToggle}>
+                ☰
+            </button>
+
+            {/* Center — Search */}
+            <div className="serach-wrapper">
+                <Search size={16} className="search-icon" />
+                <input type="text" placeholder="Explore Datasets..." className="search-input" />
+            </div>
+
+            {/* Right — Bell + User */}
+            <div className="header-right">
+                <button className="bell-btn">
+                    <Bell size={18} />
+                </button>
+
+                <div className="user-info">
+                    <div className="user-text">
+                        <span className="user-name">Julian Casablancas</span>
+                        <span className="user-role">Chief Curator</span>
+                    </div>
+                    <div className="avatar">
+                        <img src="/src/assets/User.png" alt="avatar" />
+                        <span className="avatar-dot" />
+                    </div>
+                </div>
+
+            </div>
         </header>
     )
 }
