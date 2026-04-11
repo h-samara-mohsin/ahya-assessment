144938a99ebfe2708550af966cf87baa1fdf80e0
h-samara-mohsin <samaramohsin4005617@gmail.com>
2026-04-11 23:45:46 +0500

    refactor: move Dashboard from components to pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import Sidebar from './components/Sidebar/Sidebar'
 import Header from './components/Header/Header'
-import Dashboard from './components/Dashboard/Dashboard'
+import Dashboard from './pages/Dashboard/Dashboard'
 
 
 function App() {
